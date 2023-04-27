ff84fc78fc4b609431bd708b7026d3abb5e81bd0
techlord95 <93259995+techlord95@users.noreply.github.com>
2023-04-27 14:37:58 +0530

    Update strm.py

--- app/strm.py
+++ app/strm.py
@@ -1,83 +1,9 @@
-# import streamlit as st
-# import pandas as pd
-# import pickle
-
-# # Load your trained machine learning model
-# with open('model.pkl', 'rb') as f:
-#     model = pickle.load(f)
-
-# # Create your Streamlit app UI
-# st.title('My Machine Learning App')
-# data = st.file_uploader('Upload a CSV file')
-# if data is not None:
-#     df = pd.read_csv(data)
-#     prediction = model.predict(df)
-#     st.write(prediction)
-
-# import numpy as np
-# import pickle
-# import streamlit as st
-
-
-# model=pickle.load(open("D:/classifier.pkl",'rb'))
-
-
-# def main():
-#     st.title(" Data Scientist Job Prediction ")
-    
-#     # input variables
-#     city_devevelopment_index=st.text_input("City_dev_index(eg-0.920)")
-#     education_level=st.text_input("edu_level(Graduate=2/Masters=3/High School=1/Phd=4/Primary School=0)")
-#     experience=st.text_input("Experience(year),eg-8.0")
-#     company_size=st.text_input("eg=74.5")
-#     last_new_job=st.text_input('In year(1.0)')
-#     training_hours=st.text_input('Training hours')
-#     gender_Female=st.text_input("If gender=female,add 1 else 0")
-#     gender_Male=st.text_input("If gender=Male,if yes add 1 else 0")
-#     gender_Other=st.text_input("If gender=other,add 1 or 0")
-#     relevent_experience_Hasreleventexperience=st.text_input("if yes add 1",key="re0")
-#     relevent_experience_Noreleventexperience=st.text_input("if yes add 1",key='re1')
-#     company_type_=st.text_input("0 or 1",key="type") #1
-#     type_early_Stage=st.text_input('0 or 1',key="stage")#0
-#     type_funded=st.text_input("0 or 1",key="fund")#0
-#     type_Ngo=st.text_input("0 or 1",key="ngo")#0
-#     type_other=st.text_input("0 or 1",key="ot")#0
-#     type_public=st.text_input("0 or 1",key="pvt")#0
-#     type_pvt=st.text_input("0 or 1",key="pub")#0
-#     major_disc_arts=st.text_input("0/1",key="arts")#1
-#     major_disc_bd=st.text_input("0/1",key="bd")#0
-#     major_disc_hm=st.text_input("0/1",key="hm")#0
-#     major_disc_nm=st.text_input("0/1",key="nm")#0
-#     major_disc_other=st.text_input("0/1",key="other")#0
-#     major_disc_stem=st.text_input("0/1",key="stem")#0
-#     enrolled_ftc=st.text_input("0/1",key="ftc")#1
-#     enrolled_ptc=st.text_input("0/1",key="ptc")#0
-#     # enrolled_tc=st.text_input("0/1",key="tc")#0
-    
-
-#     # gender=st.text_input('Gender(Male,Female,Other)')
-#     # enrollment=st.text_input('enrollment(no_enrollment/Full time course/Part time course)')
-#     # comp_type=st.text_input(company_)
-
-    
-    
-#     if st.button('Predict'):
-#         makeprediction=model.predict([[city_devevelopment_index,education_level,experience,company_size,last_new_job,training_hours,gender_Female,gender_Male,gender_Other,relevent_experience_Hasreleventexperience,relevent_experience_Noreleventexperience,company_type_,type_early_Stage,type_funded,type_Ngo,type_other,type_public,type_pvt,major_disc_arts,major_disc_bd,major_disc_hm,major_disc_nm,major_disc_other,major_disc_other,major_disc_stem,enrolled_ftc,enrolled_ptc]])
-#         # output=round(makeprediction[0],2)
-        
-#         st.success('Fit for the job')
-        
-    
-        
-# if __name__=='__main__':
-#     main()
-
 import numpy as np
 import pickle
 import streamlit as st
 
 
-model = pickle.load(open("D:/classifier.pkl",'rb'))
+model = pickle.load(open("classifier.pkl",'rb'))
 
 
 def main():
